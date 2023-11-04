177cdb2836fb4c365d2d8a96b09d126528d0d697
LucasAds <105825453+lucasmasternoob@users.noreply.github.com>
2023-11-04 09:47:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const generate = require('github-contributions');
+const { ContributionGraph } = require('github-contributions');
 
-generate().then(svg => {
+// Configurações para a animação da cobrinha
+const config = {
+  username: 'lucasmasternoob', // Substitua por seu nome de usuário do GitHub
+  // Adicione outras configurações necessárias, como cores, tamanho, etc.
+};
+
+// Crie a instância da ContributionGraph
+const contributionGraph = new ContributionGraph(config);
+
+// Gere a animação
+contributionGraph.generate().then((svg) => {
   const fs = require('fs');
   fs.writeFileSync('github-contribution-grid-snake.svg', svg);
 });
